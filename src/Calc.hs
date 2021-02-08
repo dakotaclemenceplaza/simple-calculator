aa328c02cc1133ed29d6d407b70228c1f060a8b2
A D <dakotaclemenceplaza@protonmail.com>
2021-02-08 17:29:08 +0400

    Add variables and tests

--- src/Calc.hs
+++ src/Calc.hs
@@ -2,63 +2,100 @@ module Calc where
 
 import System.IO (hFlush, stdout)
 import Data.Char (isSpace)
--- import Text.Read (readMaybe)
 import Data.Tuple (swap)
+import Control.Monad.State (StateT, runStateT, get, modify, lift)
 
-calc = do
+calc = run []
+
+run s = do
   putStr "> "
   hFlush stdout
   input <- getLine
   case parse input of
-    Left e -> print e >> calc
-    Right expr -> do
-      putStrLn . cleanDouble . eval $ expr
-      calc
+    Left e -> print e >> run s
+    Right i -> case runStateT (eval i) s of
+                 Left e -> print e >> run s
+                 Right (v, newS) -> do
+                   putStrLn . cleanDouble $ v
+                   run newS
+
+data Input = Assign String Expr | Eval Expr
   
 data Expr = I Double
+          | Var String
           | Add Expr Expr
           | Sub Expr Expr
           | Mul Expr Expr
           | Div Expr Expr
-  deriving (Show)
 
-eval :: Expr -> Double
-eval (Add x y) = eval x + eval y
-eval (Sub x y) = eval x - eval y
-eval (Mul x y) = eval x * eval y
-eval (Div x y) = eval x / eval y
-eval (I x) = x
+eval :: Input -> StateT [(String, Double)] (Either String) Double
+eval (Assign var expr) = do
+ v <- go expr
+ modify $ addVar (var, v)
+ pure v
+eval (Eval expr) = go expr
+
+-- helper for eval
+go :: Expr -> StateT [(String, Double)] (Either String) Double
+go (Add x y) = (+) <$> go x <*> go y
+go (Sub x y) = (-) <$> go x <*> go y
+go (Mul x y) = (*) <$> go x <*> go y
+go (Div x y) = (/) <$> go x <*> go y
+go (I x) = pure x
+go (Var var) = do
+  s <- get
+  case lookup var s of
+    Just v -> pure v
+    Nothing -> lift $ Left $ "Error: did not found variable " <> var
 
 cleanDouble = reverse . dropWhile (`elem` "0.") . reverse . show
-  
+
+addVar (variable, value) s = (variable, value) : del variable s
+  where del _ [] = []
+        del var ((v,_):xs) | v == var = xs
+        del var (v:xs) = v : del var xs
+          
 -- Not the best handling of errors
-parse :: String -> Either String Expr
-parse input | filter (not . isSpace) input == "" = Left "Empty input"
-parse input = case go (filter (not . isSpace) input) [] Nothing of
-                Nothing -> Left "Error: malformed expression"
-                Just v -> Right v
-  where -- string to parse -> list of prev exprs -> previous operation (constructor) -> resulting expression
+parse :: String -> Either String Input
+parse = parseInput . filter (not . isSpace)
+  where parseInput s | null s = Left "Empty input"
+        parseInput s = helper1 $ break (=='=') s    -- see if it has an assignment operator
+        helper1 (var, expr) | not (null var) &&                                                -- assignment branch
+                             all (`elem` ['a'..'z']) var &&
+                             not (null expr) = case go (tail expr) [] Nothing of
+                                                 Nothing -> Left "Error: malformed expression"
+                                                 Just v -> Right $ Assign var v
+        helper1 (expr, rest) | null rest = case go expr [] Nothing of                    -- expression without assignment
+                                           Nothing -> Left "Error: malformed expression"
+                                           Just v -> Right $ Eval v
+        helper1 _ = Left "Error: malformed expression"
+
+        -- parsing of expression
+        -- string to parse -> list of prev exprs -> previous operation (constructor) -> resulting expression
         go :: String -> [Maybe Expr] -> Maybe (Expr -> Expr -> Expr) -> Maybe Expr
         go "" [m] Nothing = m
         go "" [n, m] (Just op) = op <$> m <*> n
         go "" _ _ = Nothing
         go ('+':xs) (n:m:nums) (Just op) = go xs ((op <$> m <*> n) : nums) (Just Add)
-        go ('+':xs) [m] Nothing = let (rest, y) = takeNumber xs
-                                  in go rest [fmap I y, m] (Just Add)
+        go ('+':xs) [m] Nothing = let (rest, y) = takeNext xs
+                                  in go rest [y, m] (Just Add)
         go ('-':xs) (n:m:nums) (Just op) = go xs ((op <$> m <*> n) : nums) (Just Sub)
-        go ('-':xs) [m] Nothing = let (rest, y) = takeNumber xs
-                                  in go rest [fmap I y, m] (Just Sub)
-        go ('*':xs) (m:nums) op = let (rest, y) = takeNumber xs
-                                  in go rest ((Mul <$> m <*> fmap I y) : nums) op
-        go ('/':xs) (m:nums) op = let (rest, y) = takeNumber xs
-                                  in go rest ((Div <$> m <*> fmap I y) : nums) op
-        go s nums op = let (rest, n) = takeNumber s
-                       in go rest ((fmap I n) : nums) op
+        go ('-':xs) [m] Nothing = let (rest, y) = takeNext xs
+                                  in go rest [y, m] (Just Sub)
+        go ('*':xs) (m:nums) op = let (rest, y) = takeNext xs
+                                  in go rest ((Mul <$> m <*> y) : nums) op
+        go ('/':xs) (m:nums) op = let (rest, y) = takeNext xs
+                                  in go rest ((Div <$> m <*> y) : nums) op
+        go s nums op = let (rest, n) = takeNext s
+                       in go rest (n : nums) op
 
-        -- take next number
-        takeNumber :: String -> (String, Maybe Double)
-        takeNumber ('-':s) = fmap (fmap (read . ('-':))) $ helper s
-        takeNumber s = fmap (fmap read) $ helper s
-        helper (x:_) | x `notElem` "0123456789" = ("", Nothing)
-        helper s = fmap Just . swap . break (`notElem` "0123456789") $ s
-        
+        -- take next number or variable
+        takeNext :: String -> (String, Maybe Expr)
+        takeNext ('-':s) = fmap (fmap (I . read . ('-':))) $ number s
+        takeNext s = case number s of
+          (_, Nothing) -> fmap (fmap Var) . var $ s
+          v -> fmap (fmap (I . read)) v
+        number (x:_) | x `notElem` "0123456789" = ("", Nothing)
+        number s = fmap Just . swap . break (`notElem` "0123456789") $ s
+        var (x:_) | x `notElem` ['a'..'z'] = ("", Nothing)
+        var s = fmap Just . swap . break (`notElem` ['a'..'z']) $ s
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,9 +3,11 @@ module Main where
 import Test.Hspec
 import Calc (parse, eval)
 
+import Control.Monad.State (runStateT, evalStateT)
+
 main :: IO ()
 main = hspec $ do
-  describe "general tests" $ do
+  describe "tests without variables" $ do
     it "works" $ do
       
       test "23" `shouldBe` Right 23
@@ -24,7 +26,7 @@ main = hspec $ do
 
       test "2+3" `shouldBe` Right 5
       test "2+ 31" `shouldBe` Right 33
-      test "7 + 3" `shouldBe` Right 10
+      test "7 + 30" `shouldBe` Right 37
       test "25 -3" `shouldBe` Right 22
 
       test "1/5 + 1/5" `shouldBe` Right 0.4
@@ -49,5 +51,33 @@ main = hspec $ do
       test "7 *** 3 + 1 / 2" `shouldBe` Left "Error: malformed expression" 
       test "25*3 - 20 - 6 / 2 +-+ 15 * 2" `shouldBe` Left "Error: malformed expression" 
 
--- general test
-test = fmap eval . parse
+  describe "tests with variables" $ do
+    it "works" $ do
+
+      testVar "x" [("x", 3)] `shouldSatisfy` result (Right 3)
+      testVar "x + y" [("x", 3),("y", 5)] `shouldSatisfy` result (Right 8)
+      testVar "x = 5" [] `shouldSatisfy` state (Right [("x", 5)])
+      testVar "x = y + 3" [("y", 2)] `shouldSatisfy` state (Right [("x", 5), ("y", 2)])
+      testVar "x = x + 1" [("x", 2)] `shouldSatisfy` state (Right [("x", 3)])
+      
+    it "shouldn't work" $ do
+      
+      testVar "x" [] `shouldBe` Left "Error: did not found variable x"
+      testVar "3 + y" [] `shouldBe` Left "Error: did not found variable y"
+      testVar "x = 3 + y" [("x", 1), ("z", 2)] `shouldBe` Left "Error: did not found variable y"
+            
+-- test without variables
+test input = case parse input of
+  Right i -> evalStateT (eval i) []
+  Left e -> Left e   -- strange thing, simply "left -> left" doesn't work
+  
+-- test with variables
+testVar input state = case parse input of
+  Right i -> runStateT (eval i) state
+  Left e -> Left e
+
+-- helper to check the result
+result expected res = fmap fst res == expected
+
+-- helper to check the state
+state expected res = fmap snd res == expected
